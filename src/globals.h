eea62d746b1fe979ca6b504307f91e0b3dc752c0
Zack Mattor <zmattor@gmail.com>
2013-11-01 21:24:19 -0400

    changing resolution

--- src/globals.h
+++ src/globals.h
@@ -8,8 +8,8 @@
 
 enum STATE{LOADING, MENU, ABOUT, GAME, CLOSE};
 
-const int ScreenHeight = 600;
-const int ScreenWidth = 800;
+const int ScreenHeight = 840;
+const int ScreenWidth = 1200;
 
 static int TICS_PER_SEC = 60;
 static int FRAMES_PER_SEC = 60;
